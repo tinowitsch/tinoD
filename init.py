6e69f8a2a243f7914fde7070ed84f5bb3a4aeec6
tinowitsch <martinekowitsch@chello.at>
2024-05-15 00:43:33 +0200

    math

--- init.py
+++ init.py
@@ -1,18 +1,21 @@
 import pandas as pd
 import numpy as np
-
+import math
 pd.options.display.max_rows = 9999
 
 df = pd.read_csv('data/reg.csv')
 
 print(df.head())
+print(math.sqrt(81))
+print(math.pow(2, 3))
+print(math.isnan(2.2))
 
 arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
 subarr = arr[0:2, 1:3]
 print(subarr)
 subarr *= 2
 print(arr)
-#print(arr[1, 2])
+# print(arr[1, 2])
 print(np.sum(arr))
 print(np.mean(arr, axis=1))
 b = np.array([[2, 3], [4, 5], [6, 7], [8, 9]])
